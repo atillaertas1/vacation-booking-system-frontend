d7a58c739417f18865c04f1912a7c39d84521dc2
Atilla Ertaş <atillaertas@protonmail.com>
2024-10-25 04:24:15 +0300

    route'lar eklendi

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import Register from './components/Register';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import VacationForm from "./Pages/VacationForm";
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/create-spot" element={<VacationForm />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
